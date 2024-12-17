965e997f598742c06ca7d95bf7140b296a913a85
Vetphisit <vetpisit07@gmail.com>
2024-12-17 14:36:40 +0700

    lab9_2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int N,int M){
+	if(N>0&&M>0){
+		for(int i=0;i<N;i++){
+			for(int x=0;x<M;x++){0
+				cout<<"O";
+			}
+			cout<<endl;
+		}
+
+
+	}
+	else{
+		cout<<"Invalid input";
+	}
+}
 
 int main(){
 	
